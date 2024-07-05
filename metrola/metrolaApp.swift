c513b589c87aa74ca7962817d163bdd642ab2815
cheefbird <temp@mail.com>
2024-07-05 15:54:30 -0700

    initial commit 2

---
+++ metrola/metrolaApp.swift
@@ -0,0 +1,17 @@
+//
+//  metrolaApp.swift
+//  metrola
+//
+//  Created by Francis Breidenbach on 7/5/24.
+//
+
+import SwiftUI
+
+@main
+struct metrolaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ metrolaTests/metrolaTests.swift
@@ -0,0 +1,17 @@
+//
+//  metrolaTests.swift
+//  metrolaTests
+//
+//  Created by Francis Breidenbach on 7/5/24.
+//
+
+import Testing
+@testable import metrola
+
+struct metrolaTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
