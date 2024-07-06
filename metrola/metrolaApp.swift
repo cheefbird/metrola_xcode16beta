10bb23570f402f6e4e0e254336eaed73c4e93f89
cheefbird <temp@mail.com>
2024-07-05 23:31:56 -0700

    minor file cleanup

--- metrola/metrolaApp.swift
+++ metrola/metrolaApp.swift
@@ -1,14 +1,8 @@
-//
-//  metrolaApp.swift
-//  metrola
-//
-//  Created by Francis Breidenbach on 7/5/24.
-//
 
 import SwiftUI
 
 @main
-struct metrolaApp: App {
+struct MetrolaApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
